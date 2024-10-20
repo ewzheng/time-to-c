ef2e1d1a95c2738abe84bff0542a9800a5174450
SageN03 <SageAN03@gmail.com>
2024-10-19 21:45:42 -0700

    timer!!!!!!
    timer works now!

--- timer.gd
+++ timer.gd
@@ -1,28 +1,16 @@
 extends Control
 
-var countdown_duration: int = 60  # Set the countdown duration in seconds
-var timer_label: Label  # Reference to the Label node
+@onready var label = $Label
+@onready var timer  = $Timer
 
 func _ready():
-	# Get the Label node for displaying the timer
-	timer_label = $Label
-	# Set the initial time on the label
-	update_timer_label()
-
-func _on_Timer_timeout():
-	# Decrease the countdown by 1 every second
-	if countdown_duration > 0:
-		countdown_duration -= 1
-		update_timer_label()
-	else:
-		$Timer.stop()  # Stop the timer when countdown reaches 0
-		timer_finished()  # Call the function when the timer finishes
-
-# Updates the label with the formatted time
-func update_timer_label():
-	timer_label.text = str(countdown_duration)
-
-# Function to handle what happens when the timer finishes
-func timer_finished():
-	timer_label.text = "Time's Up!"
-	# You can add any additional logic here, like transitioning scenes or showing a game over screen
+	timer.start()
+	
+func time_out():
+	var time_left = timer.time_left
+	var minutes = floor(time_left / 60)
+	var seconds = int(time_left) % 60
+	return [minutes, seconds]
+	
+func _process(delta):
+	label.text = "%02d:%02d" % time_out()
