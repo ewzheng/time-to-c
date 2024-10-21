d5e3a7a2e06877f192830830383485b24feb776e
SageN03 <SageAN03@gmail.com>
2024-10-20 22:12:10 -0700

    DONE without help screen

--- game_over.gd
+++ game_over.gd
@@ -1,7 +1,7 @@
 extends Control
 
 func _on_reset_pressed() -> void:
-	get_tree().change_scene_to_file("res://Game.tscn")
+	get_tree().change_scene_to_file("res://MainMenu.tscn")
 
 
 func _on_quit_pressed() -> void:
