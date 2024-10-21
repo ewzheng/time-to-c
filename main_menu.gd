c888eda954c247ca82b5cfd2aaf63cdb8268caf4
SageN03 <SageAN03@gmail.com>
2024-10-20 22:50:41 -0700

    DONE DONE DONE

--- main_menu.gd
+++ main_menu.gd
@@ -4,9 +4,11 @@ extends Control
 func _on_start_pressed() -> void:
 	get_tree().change_scene_to_file("res://Game.tscn")
 	
-
+func _ready() -> void:
+	get_node("AudioStreamPlayer2D").play(9)
 
 func _on_quit_pressed() -> void:
 	get_tree().quit()
-	
-	
+
+func _on_help_pressed() -> void:
+	get_tree().change_scene_to_file("res://HowToPlay.tscn")
