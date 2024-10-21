adfe4cbf42a808feedee83b349ac618a6af9eaff
SageN03 <SageAN03@gmail.com>
2024-10-20 17:29:56 -0700

    new start, reset, quit button sprites

--- main_menu.gd
+++ main_menu.gd
@@ -1,7 +1,9 @@
 extends Control
 
-func _on_start_button_pressed() -> void:
+
+func _on_start_pressed() -> void:
 	get_tree().change_scene_to_file("res://Game.tscn")
 
-func _on_quit_button_pressed() -> void:
+
+func _on_quit_pressed() -> void:
 	get_tree().quit()
